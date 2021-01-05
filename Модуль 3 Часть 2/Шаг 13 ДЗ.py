5ba358755e6403a93c23818a3293bb3c33ea8d48
Xenia <aksinia.ra@gmail.com>
2021-01-05 18:49:03 +0400

    добавлен файл с выполненным домашним заданием

---
+++ Модуль 3 Часть 2/Шаг 13 ДЗ.py
@@ -0,0 +1,38 @@
+import unittest
+from selenium import webdriver
+import time
+
+
+class TestRequiredFields(unittest.TestCase):
+    def test_firstHTML(self):
+        link = "http://suninjuly.github.io/registration1.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        # Ваш код, который заполняет обязательные поля
+        elements = browser.find_elements_by_css_selector("input:required") * 1
+        for element in elements:
+            element.send_keys("Мой")
+
+        time.sleep(10)
+        # Отправляем заполненную форму
+        button = browser.find_element_by_css_selector("button.btn")
+        button.click()
+
+        # Проверяем, что смогли зарегистрироваться
+        # ждем загрузки страницы
+        time.sleep(1)
+
+        # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element_by_tag_name("h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+
+        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        assert "Congratulations! You have successfully registered!" == welcome_text
+        self.assertEqual(welcome_text, "Congratulations! You have successfully registered!",
+                         "Не все обязательыне поля заполнены!")
+
+
+if __name__ == "__main__":
+    unittest.main()
