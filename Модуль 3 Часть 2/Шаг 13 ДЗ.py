8e5c3e7ba4d250f8c7a7bc183e65945483a8a834
Xenia <aksinia.ra@gmail.com>
2021-01-07 17:28:46 +0400

    внесены правки

--- Модуль 3 Часть 2/Шаг 13 ДЗ.py
+++ Модуль 3 Часть 2/Шаг 13 ДЗ.py
@@ -37,5 +37,38 @@ class TestRequiredFields(unittest.TestCase):
         # закрываем браузер после всех манипуляций
         browser.quit()
 
+    def test_secondHTML(self):
+        link = "http://suninjuly.github.io/registration2.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        # Ваш код, который заполняет обязательные поля
+        elements = browser.find_elements_by_css_selector("input:required") * 1
+        for element in elements:
+            element.send_keys("Мой")
+
+        time.sleep(10)
+        # Отправляем заполненную форму
+        button = browser.find_element_by_css_selector("button.btn")
+        button.click()
+
+        # Проверяем, что смогли зарегистрироваться
+        # ждем загрузки страницы
+        time.sleep(1)
+
+        # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element_by_tag_name("h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+
+        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        assert "Congratulations! You have successfully registered!" == welcome_text
+        self.assertEqual(welcome_text, "Congratulations! You have successfully registered!",
+                         "Не все обязательыне поля заполнены!")
+        # ожидание чтобы визуально оценить результаты прохождения скрипта
+        time.sleep(10)
+        # закрываем браузер после всех манипуляций
+        browser.quit()
+
 if __name__ == "__main__":
     unittest.main()
